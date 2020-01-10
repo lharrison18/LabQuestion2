f6f67e3dbcd9458e4d0bf6da17f381a71c910675
Lauren Harrison <laurenharrison0322@gmail.com>
2020-01-10 15:21:30 -0700

    code

--- labQuestion2.cpp
+++ labQuestion2.cpp
@@ -1,40 +1,35 @@
 Title: Lab Question 2
 Name : Lauren Harrison
-Discription : Making an array of integers, to write a pseudocode for the choice of selection sort algorithm.
-Date : 1 / 10 / 20
+Discription : Consider an array of integers, to write a pseudocode for the choice of selection sort algorithm.
+Date : 1/10/20
 
 Selection Sort
 
 START
 
-i : = 0
-int array[5]
-srand(static_cast<int> (time(0)))
-for i = 0 to i < 5
-    rand := generatesRandomInteger [0, 100]
-    endFor
+    i : = 0
+    
+    // elements of the array
+     array[0]: = 34   
+     array[1]: = 4
+     array[2]: = 14
+     array[3]: = 28
+     array[4]: = 86
 
-    or
-
-    int array[0] = 34
-    int array[1] = 4
-    int array[2] = 14
-    int array[3] = 28
-    int array[4] = 86
-
-    for i = 1 to n - 1
+    for i = 1 to n - 1  //setting the smallest integer to the first element
         smallest = i
 
-        for j = i + 1 to n
-            if array[j] < array[smallest], then
+        for j = i + 1 to n // locating which element is the smallest
+            if array[j] < array[smallest], then  
                 smallest = j
-                endIf
-                endFor
+            endif
+        endFor
 
-                if smallestIndex != i
-                    swap array[smallest] and array[i], then
-                endIf
-          endFor
+            if smallestInteger != i // checking if the element is not equal to i's integer than it will be swapped
+                swap array[smallest] and array[i], then //swapping the integers into i's position
+            endif
+     endFor
 
+   STOP
 
-                    STOP
+                // the array I choose was 5 elememts so there would be 4 computional steps
